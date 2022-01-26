5f2faa8ce4f6f6e4ee65292a5aca77ff4d4ed0ba
LuizFerK <luizfernandoklein@live.com>
2022-01-25 21:11:55 -0300

    chore: Stitches and global styles

---
+++ web/src/styles/global.ts
@@ -0,0 +1,12 @@
+import { globalCss } from "../../stitches.config"
+
+export default globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+    backgroundColor: "$background",
+    color: "$black",
+    fontFamily: "$arial"
+  }
+})
