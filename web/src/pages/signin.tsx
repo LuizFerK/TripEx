3d66522bdec90f53e1109d69e6c1d5988ccd654a
LuizFerK <luizfernandoklein@live.com>
2022-01-27 00:45:11 -0300

    feat: SignIn page

--- web/src/pages/signin.tsx
+++ web/src/pages/signin.tsx
@@ -1,3 +1,34 @@
+import Link from "next/link"
+
+import {
+  Container,
+  Content,
+  Fieldset,
+  Label,
+  Input,
+  Button
+} from "../styles/pages/signin"
+
 export default function SignIn() {
-  return <h1>Sign In</h1>
+  return (
+    <Container>
+      <Content>
+        <h1>Sign In</h1>
+        <Link href="/signup">
+          <a>Need an account?</a>
+        </Link>
+        <form>
+          <Fieldset>
+            <Label htmlFor="username">Username</Label>
+            <Input id="username" />
+          </Fieldset>
+          <Fieldset>
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" />
+          </Fieldset>
+          <Button>Sign In</Button>
+        </form>
+      </Content>
+    </Container>
+  )
 }
---
+++ web/src/styles/pages/signin.ts
@@ -0,0 +1,67 @@
+import { styled } from "../../../stitches.config"
+
+export const Container = styled("div", {
+  display: "grid",
+  placeItems: "center",
+  height: "calc(100vh - 150px)"
+})
+
+export const Content = styled("div", {
+  width: 300,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  form: {
+    width: "100%",
+    marginTop: 30
+  }
+})
+
+export const Fieldset = styled("fieldset", {
+  all: "unset",
+  marginBottom: 15,
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start"
+})
+
+export const Label = styled("label", {
+  fontSize: 13,
+  lineHeight: 1,
+  marginBottom: 10,
+  color: "$black",
+  display: "block"
+})
+
+export const Input = styled("input", {
+  all: "unset",
+  flex: "1 0 auto",
+  borderRadius: 4,
+  padding: "0 10px",
+  fontSize: 15,
+  lineHeight: 1,
+  color: "$black",
+  border: "1px solid $secondary",
+  boxShadow: `0 0 0 1px ${"$secondary"}`,
+  height: 35,
+  "&:focus": { boxShadow: `0 0 0 2px ${"$secondary"}` }
+})
+
+export const Button = styled("button", {
+  all: "unset",
+  textAlign: "center",
+  width: "100%",
+  alignItems: "center",
+  borderRadius: 4,
+  fontSize: 15,
+  height: 35,
+  backgroundColor: "$secondary",
+  color: "$white",
+  cursor: "pointer",
+  transition: "0.2s",
+
+  "&:hover": {
+    backgroundColor: "$primary"
+  }
+})
